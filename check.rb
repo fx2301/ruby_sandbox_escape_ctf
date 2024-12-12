9e8a7c7a9ccecd60bcdb661b76dfa5f346b13245
FX 2301 <realfx2301@gmail.com>
2024-12-12 12:06:19 -0800

    First cut.

---
+++ check.rb
@@ -0,0 +1,131 @@
+require 'parser/current'
+require 'json'
+
+class SandboxException < StandardError; end;
+
+def assert_code_allowed(code, debug=false)
+  begin
+    parser = Parser::Ruby32.new
+    parser.diagnostics.consumer = lambda do |diag|
+      # Suppress parse errors
+    end
+
+    expr = parser.parse(Parser::Source::Buffer.new('(string)', source: code))
+
+    assert_node_allowed(expr, debug=debug)
+  rescue Parser::SyntaxError => e
+    # Suppress parse exceptions
+  end
+end
+
+CODE_EVAL = [:eval, :load, :module_eval, :class_eval, :instance_eval, :require, :autoload]
+SYSTEM_CALL = [:open, :exec, :system, :spawn, :popen, :capture2, :pipeline_rw, :pipeline_r, :pipeline_w, :pipeline_start, :pipeline, :popen3, :popen2, :popen2e, :capture2e, :capture3]
+BYPASSES = [:alias_method, :method, :to_proc, :instance_method, :bind_call]
+
+def assert_node_allowed(expr, debug=false)
+  return if expr.nil?
+  
+  concise = "#{expr}".gsub(/\s+/, ' ')
+  puts(">> #{concise}") if debug
+  
+  if expr.type == :xstr
+    # Backticks are system calls
+    raise SandboxException.new "System commands are not allowed. Use pure Ruby code instead."
+  elsif expr.type == :send
+    if concise.start_with?("(send (const nil :File) :open ")
+      # Allow reads of hardcoded file names
+    elsif concise.start_with?("(send (const nil :CSV) :open ")
+      # Allow calls to CSV.open
+    elsif concise.start_with?("(send (const nil :YAML) :load ")
+      # Allow YAML.load
+    else
+      method = expr.children[1]
+      assert_method_allowed(method)
+      
+      if expr.children.size >= 3
+        arg_1 = expr.children[2]
+
+        if simplify_expression(method) == :send
+          assert_method_allowed(arg_1)
+        end
+      end
+    end
+  end
+
+  if expr.respond_to?(:children)
+    expr.children.each do |expr|
+      if expr.instance_of?(::Parser::AST::Node)  
+        assert_node_allowed(expr, debug=debug)
+      end
+    end
+  end
+
+  return
+end
+
+def assert_method_allowed(method_expr)
+  if complex_expression?(method_expr)
+    raise SandboxException.new "Dynamic method calling not allowed. Use a symbol for the method you need to call."
+  end
+  method_simple = simplify_expression(method_expr)
+  if CODE_EVAL.include?(method_simple)
+    raise SandboxException.new "Dynamic code evaluation not allowed. Use code that can be statically determined to be safe."
+  elsif SYSTEM_CALL.include?(method_simple)
+    raise SandboxException.new "System commands are not allowed. Use pure Ruby code instead."
+  elsif BYPASSES.include?(method_simple)
+    raise SandboxException.new "Introspection and metaprogramming calls are limited. Use direct method calls instead."
+  end
+end
+
+def complex_expression?(expr)
+  if expr.instance_of?(::Parser::AST::Node)
+    return false if expr.type == :sym
+    return false if expr.type == :str && !expr.children[0].include?("#")
+    return true 
+  end
+  return false
+end
+
+def simplify_expression(expr)
+  if expr.instance_of?(Symbol)
+    value = expr
+  elsif expr.instance_of?(String)
+    value = expr.to_sym 
+  elsif expr.type == :str
+    value = expr.children[0].to_sym
+  elsif expr.type == :sym
+    value = expr.children[0]
+  else
+    raise "Cannot simplify: #{expr}"
+  end
+
+  value = :send if value == :public_send
+
+  return value
+end
+
+# codes = JSON.parse(File.read('code_reject.json'))
+# codes.each_with_index do |code, i|
+#   begin
+#     assert_code_allowed(code, debug=false)
+
+#     puts "#{i+1}: Expected code to be rejected:\n#{code}"
+#     assert_code_allowed(code, debug=true)
+#     exit(0)
+#   rescue SandboxException
+#   end
+# end
+
+# codes = JSON.parse(File.read('code_accept.json'))
+# codes.each_with_index do |code, i|
+#   begin
+#     assert_code_allowed(code, debug=false)
+#   rescue SandboxException
+#     puts "#{i+1}: Expected code to be accepted:\n#{code}"
+#     begin
+#       assert_code_allowed(code, debug=true)
+#     rescue
+#     end
+#     exit(0)
+#   end
+# end
---
+++ check_test.rb
@@ -0,0 +1,52 @@
+require './check'
+
+REJECTION_CASES = '''
+exec("ls")
+system("ls")
+open("|ls")
+IO.popen("ls")
+require "open3"; Open3.capture3("ls")
+spawn("ls")
+%x{ls}
+PTY.spawn("ls")
+Object.open("|ls")
+Kernel.open("|ls")
+`ls`
+eval("`ls`")
+f = "|ls"; open(f)
+Object.send(:open, "|ls")
+Object.send("open", "|ls")
+m = :open; Object.send(m, "|ls")
+Object.send("op"+"en", "|ls")
+Object.send("#{:open}", "|ls")
+Object.public_send(:open, "|ls")
+String.open("|ls")
+Object.method(:open).call("|ls")
+Object.method(:open)["|ls"]
+Object.alias_method(:foobar, :open); Object.send(:foobar, "|ls")
+:send.to_proc.call(Object, :open, "|ls")
+["|ls"].map(&Object.method(:open))
+def Object.foobaz(path); self.open(path); end; Object.foobaz("|ls")
+Object.class_eval \'open("|ls")\'
+Object.module_eval \'open("|ls")\'
+Object.new.instance_eval \'open("|ls")\'
+Object.instance_method(:open).bind_call(Object, "|ls")
+require "fiddle"; libc = Fiddle.dlopen("/lib/x86_64-linux-gnu/libc.so.6"); # ...snip...
+File.write("./hack.rb", "puts open(\'|ls\').read()"); load "./hack.rb"
+File.write("./hack.rb", "puts open(\'|ls\').read()"); autoload :Foo, "./hack.rb"; Foo.new
+File.write("./hack.rb", "puts open(\'|ls\').read()"); require_relative "hack.rb"
+IO::Buffer.new(8) # mmap, ?, profit!
+Kernel.syscall(0) # syscall, ", profit!
+'''.split("\n").reject { |line| line.strip.size == 0 }
+
+# Module.autoload
+REJECTION_CASES.each do |rejection_case|
+  begin
+    assert_code_allowed(rejection_case, debug=false)
+    puts "Expected to reject:"
+    puts rejection_case
+    assert_code_allowed(rejection_case, debug=true)
+    exit(0)
+  rescue SandboxException
+  end
+end
